cf31717c4b9bc26d98dde7bd0bcef46fcc580a76
Bahman Movaqar <Bahman@BahmanM.com>
2014-12-15 01:23:44 +0330

    Added tests and (c) header.

--- point.scm
+++ point.scm
@@ -1,7 +1,19 @@
-(load "misc")
+; Copyright 2014 Bahman Movaqar
+; 
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+; 
+;     http://www.apache.org/licenses/LICENSE-2.0
+; 
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
 
 (module point
-  (point-create point? point-coords point-coord)
+  (point-create point? point-coords point-coord point-coord-set)
   (import scheme chicken)
   (use srfi-1 misc)
   
@@ -12,5 +24,9 @@
     (make-point lis))
 
   (define (point-coord point dim)
-    (assert (and (point? point) (>= dim 1)))
-    (nth (point-coords point) dim)))
+    (assert (and (point? point) (>= dim 0)))
+    (nth (point-coords point) dim))
+
+  (define (point-coord-set point dim val)
+    (assert (and (point? point) (>= dim 0) (number? val)))
+    (point-create (nth-set (point-coords point) dim val))))
--- tests/point-tests.scm
+++ tests/point-tests.scm
@@ -1,7 +1,22 @@
+; Copyright 2014 Bahman Movaqar
+; 
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+; 
+;     http://www.apache.org/licenses/LICENSE-2.0
+; 
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+
 (use test)
 
 (test-group
  "point"
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (test-group
   "point-create"
   (test #t (let ((point (point-create (list 10 20))))
@@ -9,12 +24,30 @@
                   (point? point))))
   (test-error (point-create '()))
   (test-error (point-create 10)))
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (test-group
   "point-coords"
   (test #t (let ((point (point-create (list 10 20))))
              (and (not (null? (point-coords point)))
                   (equal? (point-coords point) (list 10 20))))))
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (test-group
   "point-coord"
   (test 10 (let ((point (point-create (list 10 20))))
-             (point-coord point 0)))))
+             (point-coord point 0)))
+  (test 20 (let ((point (point-create (list 10 20))))
+             (point-coord point 1)))
+  (test-error (let ((point (point-create (list 10 20))))
+                (point-coord point 2))))
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ (test-group
+  "point-coord-set"
+  (let ((p1 (point-create (list 10 20)))
+        (p2 (point-create (list 10 25)))
+        (p3 (point-create (list 1)))
+        (p4 (point-create (list 2))))
+    (test p2 (point-coord-set p1 1 25))
+    (test p4 (point-coord-set p3 0 2))
+    (test-error (point-coord-set 10 0 2))
+    (test-error (point-coord-set p1 -1 0))
+    (test-error (point-coord-set p1 0 (list 100 200))))))
