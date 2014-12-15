812d604823e9cca7944ac5e6c731bb6e41561fcb
Bahman Movaqar <Bahman@BahmanM.com>
2014-12-15 10:24:33 +0330

    Implemented zip-apply procedure

--- tests/misc-tests.scm
+++ tests/misc-tests.scm
@@ -16,6 +16,7 @@
 
 (test-group
  "misc"
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (test-group
   "nth-set"
   (let ((l1 (list 10 20 30))
@@ -30,6 +31,7 @@
     (test #f (equal? (nth-set l1 1 200) l1))
     (test-error (nth-set '() 1 10))
     (test-error (nth-set l1 10 1000))))
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (test-group
   "nth"
   (let ((l1 (list 10 20 30))
@@ -37,4 +39,14 @@
     (test 10 (nth l1 0))
     (test 20 (nth l1 1))
     (test-error (nth l2 1))
-    (test-error (nth '() 0)))))
+    (test-error (nth '() 0))))
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ (test-group
+  "zip-apply"
+  (test (list 1 1)
+        (zip-apply -
+                   (list 10 5) (list 9 4)))
+  (test (list 8 15)
+        (zip-apply *
+                   (list 4 3) (list 2 5)))
+  (test-error (zip-apply (lambda (x y) (* x y)) 10 20))))
